4001f3f93b43fb938ffb3f774aac6d4f59ce36ac
William Thompson <wmthmps1@gmail.com>
2023-12-29 01:23:55 -0600

    Initial Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
+        public static IEnumerable<string> SortGames(IEnumerable<string> games)
+        {
+            var sortedGameList = games.OrderBy(game => game.Length);
+            return sortedGameList;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string> {"Sonic", "Pokemon", "Call of Duty", "Minecraft", "FIFA"};
+            var newGameList = SortGames(games);
+
+            foreach (var game in newGameList)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
+
+/*
+    Create a list of video game names...
+    Order the list of games by length of the game name.
+    Use the lambda expression in this exercise as well.
+    use Method Syntax for this exercise
+
+ */
